ae99d0e7dc2a2a56167839791b97fa5e2283f87b
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-22 21:21:05 +0300

    update copyright year

--- Dev/WpfKlip/Core/Win/Helpers.cs
+++ Dev/WpfKlip/Core/Win/Helpers.cs
@@ -1,6 +1,6 @@
 ﻿#region License block
 /*
-Copyright (c) 2009 Khaprov Ilya (http://dead-trickster.com)
+Copyright (c) 2009,2015 Ilya Khaprov <ilya.khaprov@publitechs.com>
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without
